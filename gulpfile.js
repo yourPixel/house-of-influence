d810f876f47896fccb9146baff096f4492bbb645
yourPixel <40454325+yourPixel@users.noreply.github.com>
2019-10-07 10:39:47 +0300

    Add files via upload

---
+++ gulpfile.js
@@ -0,0 +1,32 @@
+var gulp = require("gulp");
+var sass = require("gulp-sass");
+const browsersync = require('browser-sync').create();
+const notifier = require('node-notifier');
+
+
+function showError(arg) {
+  notifier.notify({
+    title: 'Error',
+    message: '' + arg,
+    sound: 'Basso'
+  })
+  console.log(arg)
+}
+//compile 
+gulp.task("sass", function () { 
+	gulp.src("app/assets/scss/*.scss") 
+	.pipe(sass({
+		outputStyle: 'expanded',
+		onError: showError
+	}).on("error", function(error) {
+      showError(error)
+      sass.logError
+    })) 
+	.pipe(gulp.dest("app/assets/css")); 
+});
+
+//compile and watch 
+gulp.task("sass:watch", function() { 
+	gulp.watch("app/assets/scss/*.scss", ["sass"]);
+	gulp.watch('app/*.html', browsersync.reload)
+});
